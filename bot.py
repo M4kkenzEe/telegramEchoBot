ca614c80c8e27c18f9bf5bc4a8df3d513ce340d0
Yurii <chernigovtsev2001@mail.ru>
2021-11-04 17:35:21 +0300

    Hey

--- bot.py
+++ bot.py
@@ -1,20 +1,69 @@
 import logging
-from telegram.ext import Updater, CommandHandler, \
-    MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+from random import randint, choice
+from glob import glob
+from emoji import emojize
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 
 def greet_user(update, context):
     print('Вызван /start')
-    update.message.reply_text('Здаров')
+    update.message.reply_text(f'Че каво {smile()}\n'
+                              'Вводи /waifu пока световой меч не отсохнет!!!))))\n'
+                              'Список команд: /help\n')
 
 
 def talk_to_me(update, context):
+    print('Сообщение зеркало')
     text = update.message.text
     print(text)
-    update.message.reply_text(text)
+    update.message.reply_text(text, f'{smile()}')
+
+
+def smile():
+    smilee = choice(settings.USER_EMOJI)
+    return emojize(smilee, use_aliases=True)
+
+
+def play_random_numbers(user_number):
+    print('Вызов команды /guess')
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f'Ваше число: {user_number}, мое число: {bot_number}\nвы выиграли!'
+    elif user_number == bot_number:
+        message = f'Ваше число: {user_number}, мое число: {bot_number}\nНичья...'
+    else:
+        message = f'Ваше число: {user_number}, мое число: {bot_number}\nЯ победил, а ты проиграл))'
+    return message
+
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except(TypeError, ValueError):
+            message = 'Введите целое число'
+    else:
+        message = 'Введите число'
+    update.message.reply_text(message)
+
+
+def send_girl_pic(update, context):
+    print('Вызов команды /waifu')
+    cat_photos_list = glob('datacat/*.*')
+    cat_pic_filename = choice(cat_photos_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
+
+
+def rules(update, context):
+    update.message.reply_text('1. Сыграй с ботом в игру загадай число и введи /guess "твоё число"\n'
+                              '2. Найди свою вайфу /waifu\n---------------------------------\n'
+                              'Также напиши что то свое и бот за тобой повторит)')
 
 
 def main():
@@ -22,6 +71,9 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('help', rules))
+    dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(CommandHandler('waifu', send_girl_pic))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
@@ -29,8 +81,4 @@ def main():
     mybot.idle()
 
 
-if __name__ == 'main':
-    main()
-
-
-
+main()
